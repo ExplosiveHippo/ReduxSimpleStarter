24e5d53ba40b4097f1204af8a49efd6cf8d245ae
Lance Laughlin <lancealaughlin@gmail.com>
2017-06-11 19:15:10 -0400

    Test youtube search api

--- src/index.js
+++ src/index.js
@@ -1,9 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-
+import YTSearch from 'youtube-api-search';
 import SearchBar from './components/search_bar';
 
 const API_KEY = 'AIzaSyCuQDQwpDG4HkTObdyPPSYFzt8tq8TNZsQ';
+
+YTSearch({'key': API_KEY, 'term': 'skateboarding'}, function(data) {
+	console.log(data);
+});
 
 // Create a new componenent. It produces some HTML.
 const App = () => {
